daf7950225377c8df670398689caef10e8e31573
Radchenko Evgeniy <tdao.evg@gmail.com>
2018-10-28 20:00:54 +0500

    Read stream gitter

--- core/gateways/gitter.py
+++ core/gateways/gitter.py
@@ -12,9 +12,10 @@ import requests
 class Gitter:
     """Gitter API."""
 
-    def __init__(self, token: str, url_api: str = None, pre_text_message: str = ""):
+    def __init__(self, token: str, pre_text_message: str = ""):
         self.token = token
-        self.url_api = url_api or "https://api.gitter.im/v1"
+        self.url_rest_api = "https://api.gitter.im/v1"
+        self.url_stream_api = "https://stream.gitter.im/v1"
         self.headers = {
             "Content-Type": "application/json",
             "Accept": "application/json",
@@ -23,13 +24,30 @@ class Gitter:
         self.pre_text_message = pre_text_message
 
     def list_rooms(self):
-        """Вернет всю информацию по всем доступным комнатам"""
-        request = requests.get(f"{self.url_api}/rooms", headers=self.headers)
+        """Вернет всю информацию по всем доступным комнатам."""
+        url = f"{self.url_rest_api}/rooms"
+        request = requests.get(url, headers=self.headers)
         return request.json()
 
     def send_message_in_room(self, room_id: str, text: str):
         """Отправит сообщение в комнату."""
-        url = f"{self.url_api}/rooms/{room_id}/chatMessages"
+        url = f"{self.url_rest_api}/rooms/{room_id}/chatMessages"
         payload = {"text": f"{self.pre_text_message}{text}"}
         request = requests.post(url, data=json.dumps(payload), headers=self.headers)
         return request
+
+    def list_messages(self, room_id: str):
+        """Вернет список сообщений в комнате."""
+        url = f"{self.url_rest_api}/rooms/{room_id}/chatMessages?limit=5"
+        request = requests.get(url, headers=self.headers)
+        return request.json()
+
+    def listen_stream_messages(self, room_id: str):
+        """Вернет генератор для стрима сообщений из указанной комнаты."""
+        url = f"{self.url_stream_api}/rooms/{room_id}/chatMessages"
+        request = requests.get(url, headers=self.headers, stream=True)
+        if request.encoding is None:
+            request.encoding = "utf-8"
+        for line in request.iter_lines(decode_unicode=True):
+            if line.strip():
+                yield line

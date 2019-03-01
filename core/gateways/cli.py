63834991a1e5940113940fef7f9de9100f9b7f59
Radchenko Evgeniy <tdao.evg@gmail.com>
2019-03-02 00:55:31 +0500

    Check prefix command

--- core/gateways/cli.py
+++ core/gateways/cli.py
@@ -8,12 +8,18 @@ from core.gateways._libs import GatewayInterface
 class Gateway(GatewayInterface):
     """Command Line Interface API."""
 
-    def __init__(self):
-        pass
+    def __init__(self, signature_start_command: str = ""):
+        self.signature_start_command = signature_start_command
 
     def talk(self):
         while True:
             message_text = str(input("#")).strip()
+
+            if self.signature_start_command:
+                if not message_text.startswith(self.signature_start_command):
+                    continue
+                message_text = message_text[len(self.signature_start_command):]
+
             try:
                 result_command = handle_command(message_text)
             except UndefinedCommand:
--- core/gateways/gitter.py
+++ core/gateways/gitter.py
@@ -16,10 +16,12 @@ from core.gateways._libs import GatewayInterface
 class Gateway(GatewayInterface):
     """Gitter API."""
 
-    def __init__(self, token: str, active_room: str, signature_message_bot: str = ""):
+    def __init__(self, token: str, active_room: str, signature_message_bot: str = "",
+                 signature_start_command: str = ""):
         self.token = token
         self.active_room = active_room
         self.signature_message_bot = signature_message_bot
+        self.signature_start_command = signature_start_command
 
         self.url_rest_api = "https://api.gitter.im/v1"
         self.url_stream_api = "https://stream.gitter.im/v1"
@@ -37,6 +39,11 @@ class Gateway(GatewayInterface):
             # Если сообщение в чате исходит от бота - пропускаем обработку.
             if message_text.startswith(self.signature_message_bot):
                 continue
+
+            if self.signature_start_command:
+                if not message_text.startswith(self.signature_start_command):
+                    continue
+                message_text = message_text[len(self.signature_start_command):]
 
             try:
                 result_command = handle_command(message_text)

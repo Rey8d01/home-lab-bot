50befb9d18baa1ef61f2288ee3371ba14465cd26
rey.8d01 <rey.8d01@yandex.ru>
2021-09-03 02:43:09 +0500

    Fix run tests in drone

--- core/test/commands_test.py
+++ core/test/commands_test.py
@@ -1,4 +1,6 @@
 """Тестирование команд."""
+from itertools import product
+
 import pytest
 
 import core.commands
@@ -6,8 +8,8 @@ from core.commands import CommandResult
 from core.exceptions import CommandException
 
 
-@pytest.mark.parametrize("command", dir(core.commands))
-def test_handle_command_for_super(command, mocker):
+@pytest.mark.parametrize("command, is_super_user", product(dir(core.commands), (True, False)))
+def test_handle_command_for_user(mocker, command: str, is_super_user: bool):
     """Тестирование базового поведения обработчика для всех команд.
 
     Всякая команда должна вернуть объект результата команды либо специальное исключение.
@@ -17,8 +19,9 @@ def test_handle_command_for_super(command, mocker):
     mocker.patch("time.sleep")
     mocker.patch("core.repositories.reddit.RedditRepository.get_random_latest_post_by_subreddit")
     mocker.patch("core.repositories.bashim.get_random_quote")
+    mocker.patch("requests.get")
     try:
-        command_result = core.commands.handle_command(command, True)
+        command_result = core.commands.handle_command(command, is_super_user)
         assert isinstance(command_result, CommandResult), "result command should be type of CommandResult or raise CommandException"
     except Exception as error:
         with pytest.raises(CommandException):

389a9cb977e09dae045c7cf2879eb11e4b634ef7
rey.8d01 <rey.8d01@yandex.ru>
2021-09-03 02:45:10 +0500

    Fix run tests in drone

--- core/test/core_test.py
+++ core/test/core_test.py
@@ -1,6 +1,8 @@
+"""Тестирование функций ядра системы."""
 from config import settings
 
 
 def test_settings():
+    """Простой тест для базовых настроек."""
     gateway = settings.DEFAULT_IM
     assert gateway == "cli"
